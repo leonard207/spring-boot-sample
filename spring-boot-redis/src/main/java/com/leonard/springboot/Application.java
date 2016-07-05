2eaa6e77597fb6656c9b7138f1eab2f10b63bff1
Leonard <941668690@qq.com>
2016-07-06 01:21:10 +0800

    添加redis模块

---
+++ spring-boot-redis/src/main/java/com/leonard/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.leonard.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Leonard on 2016/5/5.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
